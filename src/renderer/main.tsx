96e1d915fcd12f6274fe091cb06f7c5d1b4c8a87
Richard Louie Orilla <Orillarichard@pm.me>
2026-09-04 23:53:09 +0800

    chore(dev): remove react inspection tooling

--- src/renderer/main.tsx
+++ src/renderer/main.tsx
@@ -5,23 +5,11 @@ import './styles/global.css'
 import {registerBlocks} from './registry/registerBlocks'
 import {useAppSettingsStore} from './store/appSettingsStore'
 
-const shouldEnableReactDevTools =
-    import.meta.env.DEV && import.meta.env.VITE_DISABLE_REACT_DEVTOOLS !== '1'
+registerBlocks();
+useAppSettingsStore.getState().loadSettings();
 
-const reactDevToolsReady = shouldEnableReactDevTools
-    ? Promise.allSettled([
-        import('react-grab'),
-        import('react-scan').then(({scan}) => scan())
-    ])
-    : Promise.resolve()
-
-void reactDevToolsReady.then(() => {
-    registerBlocks();
-    useAppSettingsStore.getState().loadSettings();
-
-    ReactDOM.createRoot(document.getElementById('root')!).render(
-        <React.StrictMode>
-            <App/>
-        </React.StrictMode>
-    );
-});
+ReactDOM.createRoot(document.getElementById('root')!).render(
+    <React.StrictMode>
+        <App/>
+    </React.StrictMode>
+);
